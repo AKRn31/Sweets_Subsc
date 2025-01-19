16ba004de5359d62d549de9b888fed1d9c62eabb
EC2 Default User <ec2-user@ip-172-31-29-78.eu-north-1.compute.internal>
2025-01-19 15:37:26 +0000

    [add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ scope module: :public do
     get 'confirm'
     patch  'withdraw'
   end
+  get '/search', to: 'searches#search'
 end  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
