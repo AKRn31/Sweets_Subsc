90eaba82f51193732b36a5c2aa87a996420560d5
EC2 Default User <ec2-user@ip-172-31-29-78.eu-north-1.compute.internal>
2025-01-20 04:07:16 +0000

    [update]検索機能

--- app/models/subsc.rb
+++ app/models/subsc.rb
@@ -2,4 +2,16 @@ class Subsc < ApplicationRecord
   has_one_attached :subsc_image
 
   has_many :posts, dependent: :destroy
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Subsc.where(name: content)
+    elsif method == 'forward'
+      Subsc.where('name LIKE ?', content+'%')
+    elsif method == 'backward'
+      Subsc.where('name LIKE ?', '%'+content)
+    else
+      Subsc.where('name LIKE ?', '%'+content+'%')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,16 @@ class User < ApplicationRecord
     image
   end
 
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(username: content)
+    elsif method == 'forward'
+      User.where('username LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('username LIKE ?', '%' + content)
+    else
+      User.where('username LIKE ?', '%' + content + '%')
+    end
+  end
  
 end
