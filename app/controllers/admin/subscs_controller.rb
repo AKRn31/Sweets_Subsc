d4e5790f9a6ee6f5d9475ad18475a8f0806513e0
EC2 Default User <ec2-user@ip-172-31-29-78.eu-north-1.compute.internal>
2025-01-27 16:34:45 +0000

    [update]管理機能

---
+++ app/controllers/admin/subscs_controller.rb
@@ -0,0 +1,14 @@
+class Admin::SubscsController < ApplicationController
+  def new
+    @subsc=Subsc.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  
   devise_for :admin, skip: [:registrations,:password], controllers: {
     sessions: 'admin/sessions'
   }
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:destroy]
+    resources :subscs, except: [:destroy]
   end
 
   scope module: :public do
