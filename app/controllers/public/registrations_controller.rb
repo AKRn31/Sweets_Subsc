e27c3c7c8afa1828d673fc3c9e25f565a3cff5a5
EC2 Default User <ec2-user@ip-172-31-29-78.eu-north-1.compute.internal>
2025-01-28 12:53:05 +0000

    [update]

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,16 +2,14 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   
-    # before_action :configure_sign_in_params, only: [:create]
-  
-    def after_sign_in_path_for(resource)
-      subscs_path
-    end
-  
-    def after_sign_out_path_for(resource)
-      root_path
-    end
-  
+  before_action :configure_sign_up_params, only: [:create]
+
+  def after_sign_in_path_for(resource)
+    subscs_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
     
   # before_action :configure_sign_up_params, only: [:create]
@@ -51,12 +49,12 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -72,4 +70,5 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+end
 
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-before_action :configure_permitted_parameters, if: :devise_controller?
+#before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     subscs_path
   end
@@ -25,10 +25,10 @@ before_action :configure_permitted_parameters, if: :devise_controller?
    
     protected
   
-    def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
-          devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
-    end
+    #def configure_permitted_parameters
+          #devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
+          #devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+    #end
   end
 
 
